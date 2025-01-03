cc18cbb3cb4bc2c64ee5c3ed94da21ee96c1bdf5
Chirag Raju <chiragrajus2002@gmail.com>
2025-01-03 18:24:26 +0530

    LinkedList Middle Of LL

---
+++ DSA-C#/LinkedList/InsertingDelLL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_C_.LinkedList
+{
+    public class InsertingDelLL
+    {
+        class Node
+        {
+            public int _data { get; set; }
+            public Node _next { get; set; }
+
+            public Node(int data,Node next) {
+                _data = data;
+                _next = next;
+            }
+            public Node(int data)
+            {
+                _data = data;
+                _next = null;
+            }
+
+        }
+
+
+
+    }
+}
---
+++ DSA-C#/LinkedList/MiddleOfLLcs.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace DSA_C_.LinkedList
+{
+    internal class MiddleOfLLcs
+    {
+        // Tortoise and Hare Algorithm to find the middle of a linked list
+
+        public class Node
+        {
+            public int Data { get; set; }
+            public Node Next { get; set; }
+
+            public Node(int data)
+            {
+                Data = data;
+                Next = null;
+            }
+
+            public Node(int data, Node next)
+            {
+                Data = data;
+                Next = next;
+            }
+        }
+
+        public class LinkedListOperations
+        {
+            // Method to find the middle of the linked list
+            public static Node FindMiddle(Node head)
+            {
+                if (head == null)
+                    return null;
+
+                Node slow = head;
+                Node fast = head;
+
+                while (fast != null && fast.Next != null)
+                {
+                    slow = slow.Next;
+                    fast = fast.Next.Next;
+                }
+
+                return slow;
+            }
+        }
+
+        // Example usage
+        public static void Main(string[] args)
+        {
+            Node head = new Node(1);
+            head.Next = new Node(2);
+            head.Next.Next = new Node(3);
+            head.Next.Next.Next = new Node(4);
+            head.Next.Next.Next.Next = new Node(5);
+
+            Node middle = LinkedListOperations.FindMiddle(head);
+            if (middle != null)
+            {
+                Console.WriteLine($"The middle node value is: {middle.Data}");
+            }
+            else
+            {
+                Console.WriteLine("The linked list is empty.");
+            }
+        }
+    }
+}
