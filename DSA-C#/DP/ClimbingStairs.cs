9d72f65d28456d8a610cbfdc2a37881b1cd9e6dd
Chirag Raju <chiragrajus2002@gmail.com>
2025-01-03 23:12:55 +0530

    Climbing stairs dp

--- DSA-C#/DP/ClimbingStairs.cs
+++ DSA-C#/DP/ClimbingStairs.cs
@@ -8,6 +8,23 @@ namespace DSA_C_.DP
 {
     internal class ClimbingStairs
     {
+       public static int Stairs(int n)
+        {
+            int[] dp = new int[n + 1];
+            dp[0] = 1;
+            dp[1] = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+            return dp[n];
+
+        }
+        public static void main(string[] args) 
+        {
+            int result = Stairs(5);
+            Console.WriteLine($"Number of ways to climb 5 stairs: {result}");
+        }
 
     }
 }
