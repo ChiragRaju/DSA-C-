61279e555e5062c6c5f77fbe2ee294cf0b8d81dd
Chirag Raju <chiragrajus2002@gmail.com>
2025-07-06 00:30:49 +0530

    DP and Array Product and Fact

---
+++ DSA-C#/Arrays/ProductofArrayExceptItself.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_C_.Arrays
+{
+    public class ProductofArrayExceptItself
+    {
+        public static int[] ProductofArrayExceptSelf(int[] nums)
+        {
+            int n = nums.Length;
+            int[] leftProducts = new int[n];
+            int[] rightProducts = new int[n];
+            int[] result = new int[n];
+            // Calculate left products
+            leftProducts[0] = 1;
+            for (int i = 1; i < n; i++)
+            {
+                leftProducts[i] = leftProducts[i - 1] * nums[i - 1];
+            }
+            // Calculate right products
+            rightProducts[n - 1] = 1;
+            for (int i = n - 2; i >= 0; i--)
+            {
+                rightProducts[i] = rightProducts[i + 1] * nums[i + 1];
+            }
+            // Calculate result
+            for (int i = 0; i < n; i++)
+            {
+                result[i] = leftProducts[i] * rightProducts[i];
+            }
+            return result;
+        }
+
+        //optimal solution
+        public static int[] ProductofArrayExceptSelfOptimal(int[] nums)
+        {
+            int n = nums.Length;
+            int[] result = new int[n];
+            result[0] = 1;
+            // Calculate left products
+            for (int i = 1; i < n; i++)
+            {
+                result[i] = result[i - 1] * nums[i - 1];
+            }
+            int rightProduct = 1;
+            // Calculate right products and multiply with left products
+            for (int i = n - 1; i >= 0; i--)
+            {
+                result[i] *= rightProduct;
+                rightProduct *= nums[i];
+            }
+            return result;
+        }
+    }
+}
---
+++ DSA-C#/DP/Factorial.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_C_.DP
+{
+    public class Factorial
+    {
+        public static int Fact(int n)
+        {
+            int[] dp=new int[n + 1];
+            dp[0]=1; // Base case: 0! = 1
+            for (int i = 1; i <= n; i++)
+            {
+                dp[i] = dp[i - 1] * i; // Calculate factorial iteratively
+            }
+            return dp[n]; // Return the factorial of n
+        }
+    }
+    //use memoization
+    public class FactorialMemoization
+    {
+        private static int[] memo;
+        public static int Fact(int n)
+        {
+            memo = new int[n + 1];
+            return FactorialHelper(n);
+        }
+        private static int FactorialHelper(int n)
+        {
+            if (n == 0 || n == 1)
+                return 1; // Base case: 0! = 1 and 1! = 1
+            if (memo[n] != 0)
+                return memo[n]; // Return cached result
+            memo[n] = n * FactorialHelper(n - 1); // Store the result in memo array
+            return memo[n];
+        }
+    }
+}
