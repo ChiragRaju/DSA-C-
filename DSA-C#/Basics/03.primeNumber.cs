fae06cdb157672179d0988387b1540441cc1d89b
Chirag Raju <chiragrajus2002@gmail.com>
2025-07-15 00:30:00 +0530

    Strings CountAndSay

---
+++ DSA-C#/Basics/03.primeNumber.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_C_.Basics
+{
+    public class primeNumber
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to find all prime numbers up to that number:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            PrintPrimesUpTo(n);
+        }
+        public static void PrintPrimesUpTo(int n)
+        {
+            for (int i = 2; i <= n; i++)
+            {
+                if (IsPrime(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        public static bool IsPrime(int number)
+        {
+            if (number <= 1)
+                return false;
+
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+    }
+}
---
+++ DSA-C#/Strings/13.CountandSay.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_C_.Strings
+{
+    public class _13
+    {
+        public static void Mains(string[] args)
+        {
+            Console.WriteLine(CountAndSay(4)); // Output: "1211"
+            Console.WriteLine(CountAndSay(5)); // Output: "111221"
+        }
+        public static string CountAndSay(int n)
+        {
+            if (n == 1)
+                return "1";
+            var Say = CountAndSay(n - 1);
+            StringBuilder result = new StringBuilder();
+            for(int i = 0; i < Say.Length; i++)
+            {
+                int count = 1;
+                char currentChar = Say[i];
+                while (i < Say.Length-1 && Say[i] == Say[i + 1])
+                {
+                    count++;
+                    i++;
+                }
+                result.Append(count).Append(currentChar);
+
+            }
+            return result.ToString();
+
+
+        }
+
+    }
+}
